76888b7abac3f59175eb7adc961cecebfb0e5406
Abhikirar <kirarabhishek75@gmail.com>
2026-01-13 16:10:57 +0530

    done

--- Advance java-JDBC/src/com/rays/jdbc/PrepareStatment/TestModel.java
+++ Advance java-JDBC/src/com/rays/jdbc/PrepareStatment/TestModel.java
@@ -1,25 +1,52 @@
 package com.rays.jdbc.PrepareStatment;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class TestModel {
 	public static void main(String[] args) throws Exception {
-		testadd();
+		 testadd();
+		// testdelete();
+		//testupdate();
+
 	}
 
+	// Add query
 	private static void testadd() throws Exception {
 		UserModal modal = new UserModal();
 		UserBean bean = new UserBean();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
-		bean.setId(2);
-		bean.setFristName("Abhishek");
-		bean.setLastName("Yadav");
-		bean.setLogin("abhishek@gmail.com");
-		bean.setPassword("Pass@1234");
-		bean.setDob(sdf.parse("2005-10-30"));
+		bean.setId(7);
+		bean.setFristName("Ranu");
+		bean.setLastName("Vyas");
+		bean.setLogin("vyas@gmail.com");
+		bean.setPassword("vyas@1234");
+		bean.setDob(sdf.parse("2002-02-21"));
 		modal.add(bean);
 
+	}
+	// delete query
+
+	private static void testdelete() throws Exception {
+		UserModal modal = new UserModal();
+		UserBean bean = new UserBean();
+
+		bean.setId(2);
+
+		modal.Delete(bean);
+	}
+
+	private static void testupdate() throws Exception {
+		UserModal modal = new UserModal();
+		UserBean bean = new UserBean();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+		bean.setId(6);
+		bean.setFristName("ram");
+		bean.setLastName("yadav");
+		bean.setLogin("yadav@gmail.com");
+		bean.setPassword("yadav@1234");
+		bean.setDob(sdf.parse("2003-02-21"));
+		modal.update(bean);
 	}
 }
--- Advance java-JDBC/src/com/rays/jdbc/PrepareStatment/UserModal.java
+++ Advance java-JDBC/src/com/rays/jdbc/PrepareStatment/UserModal.java
@@ -14,7 +14,6 @@ public class UserModal {
 		PreparedStatement pstmt = conn.prepareStatement("insert into st_user values(?,?,?,?,?,?)");
 
 		pstmt.setInt(1, bean.getId());
-
 		pstmt.setString(2, bean.getFristName());
 		pstmt.setString(3, bean.getLastName());
 		pstmt.setString(4, bean.getLogin());
@@ -29,4 +28,43 @@ public class UserModal {
 		pstmt.close();
 	}
 
+	public void Delete(UserBean bean) throws Exception {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/rays", "root", "root");
+		PreparedStatement pstmt = conn.prepareStatement("Delete from st_user where id = ?");
+
+		pstmt.setInt(1, bean.getId());
+
+		int i = pstmt.executeUpdate();
+
+		System.out.println(i + "row affected data deleted");
+
+		conn.close();
+		pstmt.close();
+
+	}
+	// update query
+
+	public void update(UserBean bean) throws Exception {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/rays", "root", "root");
+		PreparedStatement pstmt = conn.prepareStatement(
+				"update st_user set firstName = ?, lastName = ?, login = ?, password = ?, dob = ? where id =?");
+
+		pstmt.setString(1, bean.getFristName());
+		pstmt.setString(2, bean.getLastName());
+		pstmt.setString(3, bean.getLogin());
+		pstmt.setString(4, bean.getPassword());
+		pstmt.setDate(5, new java.sql.Date(bean.getDob().getTime()));
+		pstmt.setInt(6, bean.getId());
+
+		int i = pstmt.executeUpdate();
+
+		System.out.println(i + "row affected data  update");
+
+		conn.close();
+		pstmt.close();
+	}
 }
